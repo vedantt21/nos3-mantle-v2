42f7854752f8e66201f5054d8293374926e0d604
vedantt21 <vedanttyagi202@gmail.com>
2026-03-13 15:49:43 +1100

    Changed msgids and perfids for TCS component

--- components/tcs/fsw/cfs/platform_inc/tcs_msgids.h
+++ components/tcs/fsw/cfs/platform_inc/tcs_msgids.h
@@ -12,17 +12,17 @@
 /*
 ** CCSDS V1 Command Message IDs (MID) must be 0x18xx
 */
-#define TCS_CMD_MID 0x18FC /* TODO: Change this for your app */
+#define TCS_CMD_MID 0x18F1 /* TODO: Change this for your app */
 
 /*
 ** This MID is for commands telling the app to publish its telemetry message
 */
-#define TCS_REQ_HK_MID 0x18FD /* TODO: Change this for your app */
+#define TCS_REQ_HK_MID 0x18F2 /* TODO: Change this for your app */
 
 /*
 ** CCSDS V1 Telemetry Message IDs must be 0x08xx
 */
-#define TCS_HK_TLM_MID     0x08FC /* TODO: Change this for your app */
-#define TCS_DEVICE_TLM_MID 0x08FD /* TODO: Change this for your app */
+#define TCS_HK_TLM_MID     0x08F1 /* TODO: Change this for your app */
+#define TCS_DEVICE_TLM_MID 0x08F2 /* TODO: Change this for your app */
 
 #endif /* _TCS_MSGIDS_H_ */
